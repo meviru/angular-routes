9c73665d4115f1275945f5057e50ccbf3953e193
Viral Patel <viral.patel@1rivet.local>
2023-11-23 13:17:50 +0530

    feat: basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
